f14a25f40b2a2febe0d563a8909a5bb4f8d69220
Van Tian <vantian@live.com>
2020-09-24 17:10:22 +0800

    feat: 完成聊天功能主体

---
+++ Server/Hus/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatRoom.Server.Hus
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageAsync(string message) => await Clients.All.SendAsync("ReceiveMessage", message).ConfigureAwait(false);
+    }
+}
--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,5 +1,10 @@
+using System.Linq;
+
+using ChatRoom.Server.Hus;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,14 +24,21 @@ namespace ChatRoom.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddSignalR();
             services.AddControllersWithViews();
+            services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { "application/octet-stream" });
+            });
             services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseResponseCompression();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -49,6 +61,7 @@ namespace ChatRoom.Server
             {
                 endpoints.MapRazorPages();
                 endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chathub");
                 endpoints.MapFallbackToFile("index.html");
             });
         }
