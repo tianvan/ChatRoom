38d57713586693bc0da1929b6351e76e0fff529b
Van Tian <vantian@live.com>
2020-09-25 03:15:35 +0800

    feat: 增加在线人数显示功能

--- Server/Hus/ChatHub.cs
+++ Server/Hus/ChatHub.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Threading.Tasks;
 
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.SignalR;
 
 namespace ChatRoom.Server.Hus
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessageAsync(string message, DateTime sendedTime) => await Clients.All.SendAsync("ReceiveMessage", message, sendedTime).ConfigureAwait(false);
+        private int _onlineUsers;
+
+        public async Task SendMessageAsync(string message, DateTime sendedTime)
+        {
+            var userIp = Context.Features.Get<IHttpConnectionFeature>().RemoteIpAddress.ToString();
+            await Clients.All.SendAsync("MessageReceived", message, sendedTime, userIp).ConfigureAwait(false);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            _onlineUsers++;
+            await Clients.All.SendAsync("OnlineUsersChanged", _onlineUsers).ConfigureAwait(false);
+            await base.OnConnectedAsync().ConfigureAwait(false);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            _onlineUsers--;
+            await Clients.All.SendAsync("OnlineUsersChanged", _onlineUsers).ConfigureAwait(false);
+            await base.OnDisconnectedAsync(exception).ConfigureAwait(false);
+        }
     }
 }
