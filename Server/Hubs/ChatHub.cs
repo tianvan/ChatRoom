840b6f62b24981d911bc63c231ff04d412ec77ab
Van Tian <vantian@live.com>
2020-09-25 18:53:07 +0800

    feat: 用户改变事件细分为加入和离开事件

--- Server/Hubs/ChatHub.cs
+++ Server/Hubs/ChatHub.cs
@@ -25,14 +25,14 @@ namespace ChatRoom.Server.Hubs
         public override async Task OnConnectedAsync()
         {
             Interlocked.Increment(ref s_onlineUsers);
-            await Clients.All.SendAsync("OnlineUsersChanged", s_onlineUsers).ConfigureAwait(false);
+            await Clients.All.SendAsync("OnlineUserJoined", s_onlineUsers, _nicknameGenerator.Generate(Context.ConnectionId)).ConfigureAwait(false);
             await base.OnConnectedAsync().ConfigureAwait(false);
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
             Interlocked.Decrement(ref s_onlineUsers);
-            await Clients.All.SendAsync("OnlineUsersChanged", s_onlineUsers).ConfigureAwait(false);
+            await Clients.All.SendAsync("OnlineUserLeaved", s_onlineUsers, _nicknameGenerator.Generate(Context.ConnectionId)).ConfigureAwait(false);
             _nicknameGenerator.Remove(Context.ConnectionId);
             await base.OnDisconnectedAsync(exception).ConfigureAwait(false);
         }
